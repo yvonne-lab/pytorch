1078c13aeb02e2118c155e944324a9bda8b2bbbf
Marc Horowitz <mhorowitz@fb.com>
2025-01-13 19:09:47 -0800

    [dcp] Integrate stream extensions into DCP impl
    Summary: Updates FileSystemReader/Writer, Planner, DefaultLoad/SavePlanner
    
    ghstack-source-id: a0f0d705caefe18688f9c8ec4eb169aaf1bd2c9e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/143359

--- test/distributed/checkpoint/test_file_system_checkpoint_cpu.py
+++ test/distributed/checkpoint/test_file_system_checkpoint_cpu.py
@@ -20,6 +20,7 @@ from torch.distributed.checkpoint import (
     load_state_dict,
     save_state_dict,
 )
+from torch.distributed.checkpoint._extension import Rot13Example
 from torch.testing._internal.common_utils import (
     instantiate_parametrized_tests,
     parametrize,
@@ -119,6 +120,40 @@ class TestDistributedStateDictSaveLoad(TestCase):
                 assert_state_dict_equal(self, state_dict_to_load_to, state_dict_to_save)
 
             # Load from file without any resharding
+            fs_reader = FileSystemReader(path=path)
+            load_state_dict(
+                state_dict=state_dict_to_load_to,
+                storage_reader=fs_reader,
+                no_dist=True,
+            )
+
+            assert_state_dict_equal(self, state_dict_to_load_to, state_dict_to_save)
+
+
+class TestDistributedStateDictSaveLoadRot13(TestCase):
+    @parametrize("thread_count", _THREAD_COUNTS)
+    def test_read_write_only_tensor(self, thread_count) -> None:
+        with tempfile.TemporaryDirectory() as path:
+            state_dict_to_save = MyTestModule().state_dict()
+
+            fs_writer = FileSystemWriter(
+                path=path,
+                thread_count=thread_count,
+                _extensions=[Rot13Example()],
+            )
+            save_state_dict(
+                state_dict=state_dict_to_save,
+                storage_writer=fs_writer,
+                no_dist=True,
+            )
+
+            state_dict_to_load_to = MyTestModule().state_dict()
+
+            with self.assertRaises(AssertionError):
+                assert_state_dict_equal(self, state_dict_to_load_to, state_dict_to_save)
+
+            # Load from file without any resharding.  Note there is no extension
+            # specification here; it is determined dynamically from the metadata.
             fs_reader = FileSystemReader(path=path)
             load_state_dict(
                 state_dict=state_dict_to_load_to,
@@ -461,6 +496,7 @@ class TestDistributedReshardOnLoad(ShardedTensorTestBase):
 
 
 instantiate_parametrized_tests(TestDistributedStateDictSaveLoad)
+instantiate_parametrized_tests(TestDistributedStateDictSaveLoadRot13)
 instantiate_parametrized_tests(TestDistributedStateDictSaveLoadWithSharedTensor)
 instantiate_parametrized_tests(TestDistributedReshardOnLoad)
 
